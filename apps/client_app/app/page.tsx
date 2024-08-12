7037e747eb4a3846410698b5c9c8f5d2684b795b
Neerajd3vv <neerajwdev@gmail.com>
2024-08-13 00:11:26 +0530

    some-changes

--- apps/client_app/app/page.tsx
+++ apps/client_app/app/page.tsx
@@ -1,8 +1,6 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
-
-
-
+import { authOptions } from "./lib/authoptions";
 export default async function userCheck() {
   const session = await getServerSession(authOptions);
   if (session?.user) {
