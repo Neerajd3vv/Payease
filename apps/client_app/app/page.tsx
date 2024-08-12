40b38733d9fd22f1f7df770c82a9d77e5c38f90e
Neerajd3vv <neerajwdev@gmail.com>
2024-08-12 18:01:19 +0530

    testing-workflow

--- apps/client_app/app/page.tsx
+++ apps/client_app/app/page.tsx
@@ -2,6 +2,8 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "./lib/authoptions";
 
+
+
 export default async function userCheck() {
   const session = await getServerSession(authOptions);
   if (session?.user) {
